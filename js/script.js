2cc1af11eeb789f53ea92c9fc9ac6b8c3c65ad42
Hayden Harris <hayden@haydens-mbp.lan>
2021-01-06 18:46:24 -0700

    updating to master branch

--- js/script.js
+++ js/script.js
@@ -1,26 +1,75 @@
-//jquery to set variables
-let $redClick = $(".redPiece");
-let $blackClick = $(".blackPiece");
+//null are the parts where no pieces can go
+const boardStructure = [
+    null, 1, null, 2, null, 3, null, 4,
+    5, null, 6, null, 7, null, 8, null,
+    null, 9, null, 10, null, 11, null, 12,
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    13, null, 14, null, 15, null, 16, null,
+    null, 17, null, 18, null, 19, null, 20,
+    21, null, 22, null, 23, null, 24, null
+];
 
-let turn = 1;
-blackScore = 12;
-redScore = 12;
+
+//option 2 of trying to handle the click
+let currentSelection = [
+    $("#black1"), $("#black2"), $("#black3"), $("#black4"),
+    $("#black5"), $("#black6"), $("#black7"), $("#black8"),
+    $("#black9"), $("#black10"), $("#black11"), $("#black12"),
+];
+
+//setting varialbes
+let $blackClick = $(".blackPiece");
+let $redClick = $(".redPiece");
+let turn = 0;
+let blackScore = 12;
+let redScore = 12;
 let $scoreBoard = $("#score").html(`Black has ${blackScore} pieces left | Red has ${redScore} pieces left`);
 
-const rules = function (){
-    x === 1;
-    x ++;
-    return x;
+//update scoreboard function ---works but does it instantly.. need to find a good place to put it
+const updateScoreBoard = function(){
+    $scoreBoard = $("#score").html(`Black has ${blackScore} pieces left | Red has ${redScore} pieces left`);
 }
 
+//function handles click for black ---option 2. switch back to one if i can't get it working
+const blackClick = function(){
+    for (let i = 0; i < currentSelection.length; i++){
+        currentSelection[i].click(blackAlertClicked);
+        currentSelection[i].css({'border': 'solid yellow'});
+    }
+}
 
+//function handles click for red
+const redClick = function(){
+    $redClick.click(redAlertClicked);
+}
+
+//log to check its working ---delete when done
 const blackAlertClicked = function(){
     console.log(`black clicked turn = ${turn}`);
+    console.log(currentSelection);
 }
 
+//log to check its working --delete when done
 const redAlertClicked = function(){
     console.log(`red clicked turn = ${turn}`);
+    $redClick.css({'border': 'solid yellow'});
 }
 
-$redClick.click(redAlertClicked);
-$blackClick.click(blackAlertClicked);
+//function that switches players turn -----Doesn't work
+const whosTurn = function(){
+    if(turn === 0) {
+        blackClick();
+        turn = 1;
+    }
+    else {
+        console.log("red turn");
+        redClick();
+        turn = 0;
+    }
+    
+}
+
+blackClick();
+redClick();
+//console.log(currentSelection);
